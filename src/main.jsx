badccf8ca4382175a3a80a6477413fcbdf492722
seanquijote <seanquijote@gmail.com>
2023-02-14 07:37:42 +0800

    Add homepage in package.json, and replace BrowserRouter with HashRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,17 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
-import { createRoutesFromElements, createBrowserRouter, Route, RouterProvider } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
-
 import Map from "./routes/Map"
 import Error from "./Error"
 
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route
-			path="/"
-			element={<Map />}
-			errorElement={<Error />}
-		></Route>
-	)
-);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<ChakraProvider>
-			<RouterProvider router={router} />
+			<HashRouter>
+				<Map />
+			</HashRouter>
 		</ChakraProvider>
 	</React.StrictMode>
 );
